960e0f08b8eeef1b09fa887160e33c872bdeef38
Weinan Si <siweinan@gmail.com>
2021-02-18 15:56:44 -0600

    create db for test

--- env.sh
+++ env.sh
@@ -10,6 +10,12 @@ export nexusLIMS_timezone='US/Eastern'
 
 if [[ "$1" == "test" ]]; then
     export nexuslims_testing=1
+    if [[ ! -f $nexusLIMS_test_db_path ]]; then
+        echo "nexusLIMS_test_db_path -> $nexusLIMS_test_db_path not found. Creating one ..."
+        python $nexusLIMS_path/scripts/create_db.py
+        mv nexuslims_db.sqlite $nexusLIMS_test_db_path
+        [[ -f $nexusLIMS_test_db_path ]] || echo "FAIL to create test db!! Please check."
+    fi
 elif [[ $# -eq 0 && -n $nexuslims_testing ]]; then
     unset nexuslims_testing
 fi
