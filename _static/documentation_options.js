c831b3805d6202f1ca6362be0e51074916246f79
phylsix <phylsix@users.noreply.github.com>
2021-02-23 18:36:48 +0000

    deploy: b6aa90513f0fdf6506fa8546b0eea59f16e7ba52

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.post0.dev1+gea2c9ee',
+    VERSION: '0.0.post0.dev1+gb6aa905',
     LANGUAGE: 'python',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
